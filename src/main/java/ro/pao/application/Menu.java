fc6d01f71dca24aac19ece58f1b1b45b41e26c78
abianca22 <bianca-filofteia.andrei@s.unibuc.ro>
2023-04-02 02:49:51 +0300

    Instantiere obiecte si verificare metode din service

--- src/main/java/ro/pao/application/Menu.java
+++ src/main/java/ro/pao/application/Menu.java
@@ -1,12 +1,18 @@
 package ro.pao.application;
 
-import ro.pao.model.ExampleClass;
+import ro.pao.model.*;
+import ro.pao.model.enums.Invatare;
+import ro.pao.model.enums.MaterieObligatorie;
 import ro.pao.service.ExampleService;
+import ro.pao.service.ScoalaService;
+import ro.pao.service.SemestruService;
 import ro.pao.service.impl.ExampleServiceImpl;
+import ro.pao.service.impl.ScoalaServiceImpl;
+import ro.pao.service.impl.SemestruServiceImpl;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * In Meniu se fac operatiile care pot lua informatii din toate dintre servicile definite.
@@ -20,6 +26,10 @@ public class Menu {
 
     private final ExampleService exampleService = new ExampleServiceImpl();
 
+    private final ScoalaService scoalaService = new ScoalaServiceImpl();
+
+    private final SemestruService semestruService = new SemestruServiceImpl();
+
     public static Menu getInstance() {
         return (INSTANCE == null ? new Menu() : INSTANCE);
     }
@@ -28,49 +38,158 @@ public class Menu {
 
     public void intro() {
         String intro = """
-                Intro example
+                Catalog
                 """;
 
         System.out.println(intro);
 
-        ExampleClass exampleClass = ExampleClass.builder()
-                .id(UUID.randomUUID())
-                .creationDate(LocalDate.now()) // data de azi
-                .updateDate(LocalDate.now())
-                .deleteDate(LocalDate.now())
+
+        Elev elev = Elev.builder()
+                .nume("Andrei")
+                .prenume("Bianca")
+                .cnp("")
+                .adresa(new Adresa("Arges", "Pitesti", "Strada", null, null, null))
+                .stilInvatare(Invatare.PRACTIC)
+                .dataNastere(LocalDate.of(2002, 3, 22))
+                .nrMatricol(UUID.randomUUID()).build();
+
+        Scoala scoala = Scoala.builder()
+                .denumire("CNICB Pitesti")
+                .profesori(new ArrayList<>())
+                .adresa(new Adresa("Arges", "Pitesti", "Armand Calinescu", 14, null, null))
                 .build();
 
-        exampleService.addOnlyOne(exampleClass);
 
-        List<ExampleClass> exampleServiceList = List.of(
-                ExampleClass.builder()
-                        .id(UUID.randomUUID())
-                        .creationDate(LocalDate.of(2023, 03, 22))
-                        .updateDate(LocalDate.now())
+        Scoala scoala2 = Scoala.builder()
+                .denumire("Scoala")
+                .adresa(Adresa.builder()
+                        .judet("Arges")
+                        .localitate("Pitesti")
+                        .strada("Strada0")
+                        .numar(100)
+                        .build())
+                .profesori(new ArrayList<>())
+                .build();
+
+        Profesor profesor = Profesor.builder()
+                .nume("NumeProf")
+                .prenume("PrenumeProf")
+                .disciplineObligatorii(new ArrayList<>())
+                .build();
+
+        ArrayList<MaterieObligatorie> materiiPredate = new ArrayList<>();
+        materiiPredate.add(MaterieObligatorie.MATE);
+        profesor.setDisciplineObligatorii(materiiPredate);
+
+        ArrayList<Profesor> profesori = new ArrayList<>();
+        profesori.add(profesor);
+
+        scoala2.setProfesori(profesori);
+
+        Semestru semestrul1 = Semestru.builder()
+                .medieSemestriala(new HashMap<>())
+                .teze(new TreeMap<>())
+                .situatieObligatorii(new TreeMap<>())
+                .situatieOptionale(new TreeMap<>())
+                .absenteObligatorii(new TreeMap<>())
+                .absenteOptionale(new TreeMap<>())
+                .build();
+
+        Semestru semestrul2 = Semestru.builder()
+                .medieSemestriala(new HashMap<>())
+                .teze(new TreeMap<>())
+                .situatieObligatorii(new TreeMap<>())
+                .situatieOptionale(new TreeMap<>())
+                .absenteObligatorii(new TreeMap<>())
+                .absenteOptionale(new TreeMap<>())
+                .build();
+
+
+
+
+        Catalog catalog = Catalog.builder()
+                .scoala(scoala)
+                .clasa(5)
+                .literaClasa('c')
+                .elevi(new HashMap<UUID, Elev>())
+                .profesoriClasa(profesori)
+                .materiiObligatorii(materiiPredate)
+                .semestru1(semestrul1)
+                .semestru2(semestrul2)
+                .build();
+
+        System.out.println("Inainte de adaugare: " + scoalaService.listeazaLista() + "\n\n");
+
+        scoalaService.adaugaScoala(scoala);
+
+        scoalaService.adaugaScoli(List.of(
+                Scoala.builder()
+                        .denumire("Scoala7")
+                        .adresa(
+                                Adresa.builder()
+                                        .judet("Arges")
+                                        .localitate("Pitesti")
+                                        .strada("Strada2")
+                                        .build()
+                        )
+                        .profesori(new ArrayList<>())
                         .build(),
-                ExampleClass.builder()
-                        .id(UUID.randomUUID())
-                        .creationDate(LocalDate.of(2023, 03, 22))
-                        .updateDate(LocalDate.now())
+                Scoala.builder()
+                        .denumire("Scoala5")
+                        .adresa(
+                                Adresa.builder()
+                                        .judet("Arges")
+                                        .localitate("Pitesti")
+                                        .strada("Strada3")
+                                        .build()
+                        )
+                        .profesori(new ArrayList<>())
                         .build()
-        );
+        ));
 
-        exampleService.addAllFromGivenList(exampleServiceList);
+        System.out.println("Dupa adaugare: " + scoalaService.listeazaLista() + "\n\n");
 
-        System.out.println("Inainte de stergere: ");
-        exampleService.getAllFromList()
-                .forEach(elementFromList -> System.out.println(elementFromList));
+        System.out.println("Lista scolilor inainte de sortare: ");
+        System.out.println(scoalaService.listeazaLista() + "\n\n");
+        scoalaService.sorteazaNume();
+        System.out.println("Lista scolilor dupa sortare: ");
+        System.out.println(scoalaService.listeazaLista() + "\n\n");
+
+        scoalaService.stergeScoala(scoala);
+
+        System.out.println("Dupa stergere: " + scoalaService.listeazaLista() + "\n\n");
+
+        System.out.println("Adaugam obiectul sters anterior din nou");
+        scoalaService.adaugaScoala(scoala);
+        System.out.println(scoalaService.listeazaLista() + "\n\n");
+
+        System.out.println("Modificam acest obiect");
+
+        scoalaService.modificaScoala(scoala, scoala2);
+        System.out.println("Dupa modificare: " + scoalaService.listeazaLista() + "\n\n");
 
 
-        System.out.println("Dupa modificare: ");
-        exampleClass.setUpdateDate(LocalDate.of(2, 2, 2));
-        exampleService.modificaElementById(exampleClass.getId(), exampleClass);
-        exampleService.getAllFromList()
-                .forEach(elementFromList -> System.out.println(elementFromList));
+        System.out.println("Catalog " + catalog + "\n\n");
 
-        System.out.println("Dupa stergere: ");
-        exampleService.removeElementById(exampleClass.getId());
-        exampleService.getAllFromList()
-                .forEach(elementFromList -> System.out.println(elementFromList));
+        System.out.println("Adaugam elev in catalog");
+        semestruService.adaugaElev(catalog, elev);
+        System.out.println("Elevi: " + catalog.getElevi() + "\n\n");
+        System.out.println("Situatie elevi primul semestru: " + catalog.getSemestru1().getSituatieObligatorii() + "\n\n");
+        System.out.println("Punem elevului adaugat nota 10 la Matematica pe primul semestru");
+        semestruService.adaugaNotaObl(catalog, 1, MaterieObligatorie.MATE, new Nota(10, LocalDate.now()), elev);
+        System.out.println("Nota elevului " + elev.getNume() + " " + elev.getPrenume() + " este " + catalog.getSemestru1()
+                .getSituatieObligatorii()
+                .get(elev)
+                .get(MaterieObligatorie.MATE)
+                .get(0) + "\n\n");
+        System.out.println("Stergem elevul din catalog");
+        semestruService.stergeElev(catalog, elev);
+        System.out.println("Elevii din catalog dupa stergere: " + catalog.getElevi() + "\n\n");
+        System.out.println("Situatia scoalara a eleviloe de pe primul semestru dupa stergere: " + catalog.getSemestru1().getSituatieObligatorii() + "\n\n");
+
+
+
+
+
     }
 }
