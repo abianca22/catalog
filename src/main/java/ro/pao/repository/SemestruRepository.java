b0b742a918e1ad01abaa8a5124c68662495f2155
abianca22 <bianca-filofteia.andrei@s.unibuc.ro>
2023-05-25 23:20:45 +0300

    Creare interfata SemestruRepository

---
+++ src/main/java/ro/pao/repository/SemestruRepository.java
@@ -0,0 +1,20 @@
+package ro.pao.repository;
+
+import ro.pao.model.ExampleClass;
+import ro.pao.model.Semestru;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SemestruRepository {
+    Optional<Semestru> getSEmestruById(UUID id);
+
+    void deleteSemestruById(UUID id);
+
+    void updateSemestruById(UUID id, Semestru newSemestru);
+
+    void addNewSemestru(Semestru semestru);
+
+    List<Semestru> getAll();
+}
