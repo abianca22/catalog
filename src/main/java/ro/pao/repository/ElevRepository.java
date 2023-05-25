e253a5a234623dadce5cd479689fdc1cda5fe686
abianca22 <bianca-filofteia.andrei@s.unibuc.ro>
2023-05-25 23:10:47 +0300

    Creare interfata ElevRepository

---
+++ src/main/java/ro/pao/repository/ElevRepository.java
@@ -0,0 +1,22 @@
+package ro.pao.repository;
+
+import ro.pao.model.Elev;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ElevRepository {
+    // dupa numarul matricol
+    Optional<Elev> getElevById(UUID id);
+
+    void deleteElevById(UUID id);
+
+    void updateElevById(UUID id, Elev elevNou);
+
+    void addNewElev(Elev elev);
+
+    List<Elev> getAll();
+
+    void addAllFromGivenList(List<Elev> listaElevi);
+}
