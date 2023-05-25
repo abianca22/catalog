5fd897aa07baeeeecff1fcfedc11942a17bf4969
abianca22 <bianca-filofteia.andrei@s.unibuc.ro>
2023-05-25 23:11:09 +0300

    Creare interfata ProfesorRepository

---
+++ src/main/java/ro/pao/repository/ProfesorRepository.java
@@ -0,0 +1,22 @@
+package ro.pao.repository;
+
+import ro.pao.model.ExampleClass;
+import ro.pao.model.Profesor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProfesorRepository {
+    Optional<Profesor> getProfById(UUID id);
+
+    void deleteProfById(UUID id);
+
+    void updateProfById(UUID id, Profesor newProf);
+
+    void addNewProf(Profesor profesor);
+
+    List<Profesor> getAll();
+
+    void addAllFromGivenList(List<Profesor> listaProfesori);
+}
