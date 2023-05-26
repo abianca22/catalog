ac16e34721e310fac076bfb24011d4added5ad55
abianca22 <bianca-filofteia.andrei@s.unibuc.ro>
2023-05-26 14:16:27 +0300

    ElevService && ElevServiceImpl

---
+++ src/main/java/ro/pao/service/ElevService.java
@@ -0,0 +1,22 @@
+package ro.pao.service;
+
+import ro.pao.model.Elev;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ElevService {
+    Optional<Elev> getById(UUID id);
+
+    List<Elev> getAll();
+
+    void deleteById(UUID id);
+
+    void updateNumeById(UUID id, Elev elev);
+
+    void updatePrenumeById(UUID id, Elev elev);
+
+    void add(Elev elev);
+
+}
---
+++ src/main/java/ro/pao/service/impl/ElevServiceImpl.java
@@ -0,0 +1,48 @@
+package ro.pao.service.impl;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import ro.pao.model.Elev;
+import ro.pao.repository.ElevRepository;
+import ro.pao.service.ElevService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Getter
+public class ElevServiceImpl implements ElevService {
+
+    private final ElevRepository elevRepository;
+
+    @Override
+    public Optional<Elev> getById(UUID id){
+        return elevRepository.getElevById(id);
+    }
+
+    @Override
+    public List<Elev> getAll(){
+        return elevRepository.getAll();
+    }
+
+    @Override
+    public void deleteById(UUID id){
+        elevRepository.deleteElevById(id);
+    }
+
+    @Override
+    public void updateNumeById(UUID id, Elev elev){
+        elevRepository.updateNumeById(id, elev);
+    }
+
+    @Override
+    public void updatePrenumeById(UUID id, Elev elev){
+        elevRepository.updatePrenumeById(id, elev);
+    }
+
+    @Override
+    public void add(Elev elev){
+        elevRepository.addNewElev(elev);
+    }
+}
