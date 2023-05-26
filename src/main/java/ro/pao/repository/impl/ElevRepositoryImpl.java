70f6a484f47f1b3902e51f5dd9fcf304f3a5bd97
abianca22 <bianca-filofteia.andrei@s.unibuc.ro>
2023-05-26 03:59:29 +0300

    implementare repository Elev

---
+++ src/main/java/ro/pao/repository/impl/ElevRepositoryImpl.java
@@ -0,0 +1,130 @@
+package ro.pao.repository.impl;
+
+import ro.pao.config.DatabaseConfiguration;
+import ro.pao.mapper.AdresaMapper;
+import ro.pao.mapper.ElevMapper;
+import ro.pao.model.Adresa;
+import ro.pao.model.Elev;
+import ro.pao.model.ExampleClass;
+import ro.pao.repository.AdresaRepository;
+import ro.pao.repository.ElevRepository;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ElevRepositoryImpl implements ElevRepository {
+
+    private static final ElevMapper elevMapper = ElevMapper.getInstance();
+
+    private static final AdresaMapper adresaMapper = AdresaMapper.getInstance();
+
+    @Override
+    public Optional<Elev> getElevById(UUID id){
+        String selectSql = "SELECT * FROM elev WHERE nr_matricol=?";
+        String selectAdresa = "SELECT * FROM adresa WHERE id=?";
+
+        try (Connection connection = DatabaseConfiguration.getDatabaseConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(selectSql);
+             PreparedStatement preparedStatementAdresa = connection.prepareStatement(selectAdresa)) {
+            preparedStatement.setString(1, id.toString());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            String idAdresa = resultSet.getString(1);
+            preparedStatementAdresa.setString(1, idAdresa);
+
+            ResultSet resultSetAdresa = preparedStatementAdresa.executeQuery();
+
+            Optional<Adresa> adresa = adresaMapper.mapToAdresa(resultSetAdresa);
+
+            return elevMapper.mapToElev(resultSet, (adresa.isPresent()) ? adresa.get() : null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteElevById(UUID id) {
+        String deleteSql = "DELETE FROM elev WHERE nr_matricol=?";
+
+        try (Connection connection = DatabaseConfiguration.getDatabaseConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteSql)){
+            preparedStatement.setString(1, id.toString());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateNumeById(UUID id, Elev newElev) {
+        String updateSql = "UPDATE elev SET nume=? WHERE nr_matricol=?";
+
+        try(Connection connection = DatabaseConfiguration.getDatabaseConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(updateSql)){
+            preparedStatement.setString(1, newElev.getNume());
+            preparedStatement.setString(2, id.toString());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updatePrenumeById(UUID id, Elev newElev) {
+        String updateSql = "UPDATE elev SET prenume=? WHERE nr_matricol=?";
+
+        try(Connection connection = DatabaseConfiguration.getDatabaseConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(updateSql)){
+            preparedStatement.setString(1, newElev.getPrenume());
+            preparedStatement.setString(2, id.toString());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addNewElev(Elev elev) {
+        String insertSql = "INSERT INTO elev VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConfiguration.getDatabaseConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertSql)){
+            preparedStatement.setString(1, elev.getNrMatricol().toString());
+            preparedStatement.setString(2, elev.getNume());
+            preparedStatement.setString(3, elev.getPrenume());
+            preparedStatement.setString(4, elev.getCnp());
+            preparedStatement.setDate(5, Date.valueOf(elev.getDataNastere()));
+            preparedStatement.setString(6, elev.getStilInvatare().toString());
+            preparedStatement.setString(7, elev.getAdresa().getId().toString());
+
+            preparedStatement.executeUpdate(insertSql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Elev> getAll() {
+        String selectSql = "SELECT * FROM elev e, adresa a WHERE e.id_adresa = a.id";
+
+        try (Connection connection = DatabaseConfiguration.getDatabaseConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(selectSql)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return elevMapper.mapToElevList(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return List.of();
+
+    }
+
+
+}
